f191089ae12c251ff5cf8a79300ba530f601aaf8
benedekpal <benedek.pal1024@gmail.com>
2025-07-05 21:18:17 +0200

    recursive function that copies all the contents from a source directory to a destination directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 from textnode import TextNode
 from textnode import TextType, TextNode
+import os
+import shutil
+
+def copy_folder_content(source, target):
+    if os.path.exists(target):
+        shutil.rmtree(target)
+    os.mkdir(target)
+    if os.path.exists(source):
+        contents = os.listdir(source)
+        for content in contents:
+            source_path = os.path.join(source, content)
+            target_path = os.path.join(target, content)
+            if os.path.isfile(source_path):
+                print(f"Copying {source_path} --> {target}")
+                shutil.copy2(source_path, target)
+            else:
+                copy_folder_content(source_path, target_path)
+
 
 def main():
+    copy_folder_content("./static", "./public")
+
     new_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(str(new_node))
 
